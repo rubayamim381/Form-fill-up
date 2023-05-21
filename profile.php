ff783c5b1dbecad2c176d77d8274b304da3ff790
Rubaya Akter Mim <45107866+rubayamim381@users.noreply.github.com>
2023-05-22 00:45:43 +0600

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,9 +1,7 @@
 <?php
 include "servers.php";
 
-
 $email1 = $_SESSION['email'];
-
 $join = $con->prepare("SELECT user.username, information.* FROM user, information WHERE user.email = information.email AND user.email = ?;");
 $join->bind_param("s", $email1);
 $join->execute();
@@ -11,22 +9,7 @@ $JoiningResult = $join->get_result();
 $join->close();
 
 
-/*$join->bind_param("s", $email);
-$join->execute();
-$join->close();
-*/
-
-
-//SELECT user.email, information.* FROM user INNER JOIN information ON (user.email = information.email);"
-//SELECT * FROM information WHERE email = ?
-//SELECT user.username, information.* FROM user, information WHERE user.id = info_num AND email = $email ORDER BY user.id;;
-
-//$JoiningResult =
-//(mysqli_num_rows($JoiningResult)) > 0;
-
 if ($JoiningResult) {
-  // output data of each row
-  //while($row = $JoiningResult->fetch_array())
 
   $JoiningResult->field_seek(1);
   $fileInfo = $JoiningResult->fetch_assoc();
